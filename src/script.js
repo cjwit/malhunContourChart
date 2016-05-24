39202d3d654ebc335c71d7ca8e26f7bcba98f35b
cjwit <chris.witulski@gmail.com>
2016-05-23 20:39:28 -0400

    add rep, change color to category20()

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ var data,
     margin = { top: 20, right: 20, bottom: 30, left: 30},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
-    color = d3.scale.category10(),
+    color = d3.scale.category20(),
     titleArtist = [],
     selected = [];
 
